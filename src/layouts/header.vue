8910c8e26dfadca9cbe3e6520a7ed5ef14f8fa9f
AQian <970506352@qq.com>
2023-05-08 11:24:23 +0800

    feat: 添加跳转首页功能

--- src/layouts/header.vue
+++ src/layouts/header.vue
@@ -3,7 +3,12 @@
     <div class="logo"></div>
     <div class="navArray">
       <template v-for="(navItem, index) of navArray">
-        <div v-if="navItem" :key="index" class="navItem">
+        <div
+          v-if="navItem"
+          :key="index"
+          class="navItem"
+          @click="handleNav(navItem)"
+        >
           {{ navItem }}
         </div>
         <ElDivider v-else direction="vertical"></ElDivider>
@@ -42,6 +47,14 @@ const iconSize = ref<number>(20);
 const avatarSize = computed(() => {
   return (iconSize.value * 8) / 5;
 });
+const handleNav = (nav: string) => {
+  const routeObject: { [key: string]: string } = {
+    首页: "/",
+  };
+  return navigateTo({
+    path: routeObject[nav],
+  });
+};
 const handleIcon = (icon: unknown) => {
   const functionObject: { [key: string]: () => void } = {
     // Setting: goSetting,
