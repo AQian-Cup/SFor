85e5a6bcb17e616412edc98bbb38bb090b120c5d
AQian <970506352@qq.com>
2023-05-23 16:21:13 +0800

    feat: 实现events页面下的post组件功能

---
+++ src/components/events/post/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <ElCard class="post" shadow="hover" @click="goEvent">
+    <template #header>
+      <div class="header">
+        <div class="title">{{ title }}</div>
+        <div class="source">{{ source }}</div>
+      </div>
+    </template>
+    <div class="content">{{ content }}</div>
+  </ElCard>
+</template>
+<script lang="ts" setup>
+interface postType {
+  id: string | number;
+  title: string;
+  content: string;
+  source: string;
+}
+const props = defineProps<postType>();
+const goEvent = () => {
+  return navigateTo({
+    path: `/event/${props.id}`,
+  });
+};
+</script>
+<style scoped>
+.post {
+  height: 200px;
+  cursor: pointer;
+}
+.post + .post {
+  margin-top: 20px;
+}
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.title {
+  font-weight: bold;
+}
+</style>
