23dc5055c9351d8124338c07b028d3261964ac92
AQian <970506352@qq.com>
2023-05-26 16:59:21 +0800

    feat: 实现create接口

---
+++ src/server/api/event/create.ts
@@ -0,0 +1,23 @@
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const count = await useMongodb(
+    "SFor",
+    "event",
+  ).collectionOrigin.countDocuments();
+  return await useMongodb("SFor", "event").setItem({
+    id: count + 1,
+    university: body.university,
+    college: body.college,
+    credit: body.credit,
+    startTime: body.startTime,
+    endTime: body.endTime,
+    introdution: body.introdution,
+    paricipants: [],
+    name: body.eventName,
+    source: {
+      name: body.name,
+      id: body.id,
+    },
+    images: [],
+  });
+});
